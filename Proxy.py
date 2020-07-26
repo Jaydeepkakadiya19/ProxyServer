fa155452dcbe4961c99948029e89dc71bf2cdc8e
Jaydeepkakadiya19 <jaykakadiya19@gmail.com>
2020-07-26 11:53:03 +0530

    socket done

--- Proxy.py
+++ Proxy.py
@@ -1,23 +1,55 @@
-import socket  # for socket
+import socket
 import sys
+import threading
+import _thread
 
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket successfully created")
-except socket.error as err:
-    print("socket creation failed with error %s" + err)
-
-
-port = 80
-
-try:
-    host_ip = socket.gethostbyname('www.google.com')
-except socket.gaierror:
-
-    print("there was an error resolving the host")
+    port = int(input("Enter port number: "))
+except KeyboardInterrupt:
+    print("User Interupt")
+    print("Application exiting.. ")
     sys.exit()
 
-s.connect((host_ip, port))
 
-print("the socket has successfully connected to google \
-      on port == %s" % (host_ip))
+max_conn = 5
+buffer_size = 4096
+
+
+def start():
+    try:
+        Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        Socket.bind('', port)
+        Socket.listen(max_conn)
+        print("Initializing  socket...  Done")
+        print("Sockets binded successfully...")
+        print("server started successfully %d \n " % (port))
+    except Exception, e:
+        print("Unable to Initialize Socket...")
+        sys.exit(2)
+
+    while True:
+        try:
+            (conn, addr) = Socket.accept()
+            data = conn.recv(buffer_size)
+            _thread.start_new_thread(conn_string, (conn, data, addr))
+        except KeyboardInterrupt:
+            Socket.close()
+            print("Proxy Server Shutting down")
+            sys.exit(1)
+
+    Socket.close()
+
+
+def conn_string(conn, data, addr):
+    try:
+        print(data)
+        first_line = data.split('\n')[0]
+        url = first_line.split(' ')[1]
+        https_pos = url.find("://")
+        if(https_pos == -1):
+            temp = url
+        else:
+            temp = url[(https_pos+3):]
+
+        port_pos = temp.find(":")
+        webserver_pos = temp.find("/")
